04aece225ded8c5bd48e01bd8621fa0c41ee8bde
Johnny Matthews <johnny.matthews@gmail.com>
2025-03-05 12:46:33 -0600

    Formatting

--- source/blender/makesrna/intern/rna_object.cc
+++ source/blender/makesrna/intern/rna_object.cc
@@ -2956,15 +2956,11 @@ static void rna_def_object(BlenderRNA *brna)
   RNA_def_property_update(prop, NC_OBJECT | ND_DRAW, "rna_Object_dependency_update");
 
   prop = RNA_def_property(srna, "parent_use_final_geometry", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(
-    prop, nullptr, "transflag", OB_PARENT_USE_FINAL_GEOMETRY);
+  RNA_def_property_boolean_sdna(prop, nullptr, "transflag", OB_PARENT_USE_FINAL_GEOMETRY);
   RNA_def_property_ui_text(prop,
-                         "Use Final Geometry",
-                         "Use the final evaluated indices rather than "
-                         "the original geometry"
-                         );
+                           "Use Final Geometry",
+                           "Use the final evaluated indices rather than the original geometry");
   RNA_def_property_update(prop, NC_OBJECT | ND_DRAW, "rna_Object_internal_update");
-
 
   prop = RNA_def_property(srna, "use_camera_lock_parent", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(
