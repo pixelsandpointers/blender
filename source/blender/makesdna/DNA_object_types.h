8cf80a9799b610f486696843dbaead241b7a3c6b
Johnny Matthews <johnny.matthews@gmail.com>
2025-03-05 12:49:36 -0600

    Add comment

--- source/blender/makesdna/DNA_object_types.h
+++ source/blender/makesdna/DNA_object_types.h
@@ -577,7 +577,7 @@ enum {
   OB_TRANSFLAG_UNUSED_12 = 1 << 12, /* cleared */
   /* runtime constraints disable */
   OB_NO_CONSTRAINTS = 1 << 13,
-
+  /* when calculating vertex parent position, ignore CD_ORIGINDEX layer */
   OB_PARENT_USE_FINAL_GEOMETRY = 1 << 14,
 
   OB_DUPLI = OB_DUPLIVERTS | OB_DUPLICOLLECTION | OB_DUPLIFACES | OB_DUPLIPARTS,
